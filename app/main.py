c027c1359f57357266add032145644248d8ad455
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-17 14:38:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -11,31 +11,38 @@ class BookProcessor:
     def process(self, commands: list[tuple[str, str]]) -> None | str:
         for cmd, method_type in commands:
             if cmd == "display":
-                if method_type == "console":
-                    strategy = ConsoleDisplayStrategy()
-                elif method_type == "reverse":
-                    strategy = ReverseDisplayStrategy()
-                else:
-                    raise ValueError(f"Unknown display type: {method_type}")
-                strategy.display(self.book)
-
+                self._process_display(method_type)
             elif cmd == "print":
-                if method_type == "console":
-                    strategy = ConsolePrintStrategy()
-                elif method_type == "reverse":
-                    strategy = ReversePrintStrategy()
-                else:
-                    raise ValueError(f"Unknown print type: {method_type}")
-                strategy.print_book(self.book)
-
+                self._process_print(method_type)
             elif cmd == "serialize":
-                if method_type == "json":
-                    strategy = JSONSerializeStrategy()
-                elif method_type == "xml":
-                    strategy = XMLSerializeStrategy()
-                else:
-                    raise ValueError(f"Unknown serialize type: {method_type}")
-                return strategy.serialize(self.book)
+                return self._process_serialize(method_type)
+
+    def _process_display(self, method_type: str) -> None:
+        if method_type == "console":
+            strategy = ConsoleDisplayStrategy()
+        elif method_type == "reverse":
+            strategy = ReverseDisplayStrategy()
+        else:
+            raise ValueError(f"Unknown display type: {method_type}")
+        strategy.display(self.book)
+
+    def _process_print(self, method_type: str) -> None:
+        if method_type == "console":
+            strategy = ConsolePrintStrategy()
+        elif method_type == "reverse":
+            strategy = ReversePrintStrategy()
+        else:
+            raise ValueError(f"Unknown print type: {method_type}")
+        strategy.print_book(self.book)
+
+    def _process_serialize(self, method_type: str) -> str:
+        if method_type == "json":
+            strategy = JSONSerializeStrategy()
+        elif method_type == "xml":
+            strategy = XMLSerializeStrategy()
+        else:
+            raise ValueError(f"Unknown serialize type: {method_type}")
+        return strategy.serialize(self.book)
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
